1654aa8533fd7c63aabb574bb9f13f5af7cfae48
saylesc <cory.sayles@gmail.com>
2021-03-16 22:31:07 -0400

    Added the 'C' in CRUD operations - which adds a TODO to our DB

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/udacity'
@@ -7,6 +8,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Defines the ToDo Model for our database
 class Todo(db.Model):
    __tablename__ = 'todos'
    id = db.Column(db.Integer, primary_key=True)
@@ -19,6 +21,44 @@ class Todo(db.Model):
 # Create all tables
 db.create_all()
 
+
+######## APP ROUTES #######
 @app.route('/')
 def index():
-   return render_template('index.html', data = Todo.query.all())
+   return render_template( 'index.html', data = Todo.query.all() )
+
+@app.route('/todos/create-todo', methods=['POST'])
+def create():
+   # Get the form data here
+   newTodoItem = request.get_json()['description']
+
+   #### OLD Way using form
+   # newTodoItem = request.form.get( 'description', 'New todo with no description' )
+
+   # Try to add the TODO item. If failure, rollback and report error
+   error = False
+   body = {}
+   try: 
+      newTodo = Todo( description=newTodoItem )
+
+      # add the new todo item to the db and commit
+      db.session.add( newTodo )
+      db.session.commit()
+      body['description'] = newTodo.description
+   except:
+      error = True
+      db.session.rollback()
+      print(sys.exc_info())
+   finally:
+      db.session.close()
+   
+   if error:
+      # Throw an HTTP Exception
+      abort(500)
+   else:
+      return jsonify(body)
+   #### OLD Way using redirect
+   # Redirect to the main page
+   # return redirect( url_for('index') )
+
+   
