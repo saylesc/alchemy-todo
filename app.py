f64ba9b6cd28f4782b96fd0909760640e9b4eedc
saylesc <cory.sayles@gmail.com>
2021-03-28 19:02:18 -0400

    Upgraded DB using migration. Added TodoList logic

--- app.py
+++ app.py
@@ -10,16 +10,44 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# Defines the TodoList (parent of Todo's) Model for our database
+class TodoList(db.Model):
+   __tablename__= 'todolist'
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(), nullable=False)
+   todos = db.relationship(
+      'Todo', 
+      backref='todo_list', 
+      lazy=True)
+      # collection_class='list',
+      # cascade='save-update')
+
+   # Define the debug data to display
+   def __repr__(self):
+      return f'<TodoList {self.id} {self.name}>'
+
 # Defines the ToDo Model for our database
 class Todo(db.Model):
    __tablename__ = 'todos'
    id = db.Column(db.Integer, primary_key=True)
    description = db.Column(db.String(), nullable=False)
    completed = db.Column(db.Boolean, nullable=False, default=False)
+   todolist_id = db.Column(
+      db.Integer, 
+      db.ForeignKey('todolist.id'),
+      nullable=False)
+   
+   # Add new todos and associate them with a list as follows:
+   # list = TodoList(name='My List')
+   # todo = Todo(description='My Todo Item')
+   # ...
+   # todo.todo_list = list
+   # db.session.add(list) - takes care of cascading the child todos
+   # db.session.commit()
 
    # Define the debug data to display
    def __repr__(self):
-      return f'<TODO {self.id} {self.description}>'
+      return f'<TODO {self.id} {self.description}, list {self.todolist_id}>'
 
 # Create all tables
 # Learned how to use migration, so this command is no longer needed
@@ -31,12 +59,14 @@ class Todo(db.Model):
 def create():
    # Get the form data here
    newTodoItem = request.get_json()['description']
+   listId = request.get_json()['listId']
 
    # Try to add the TODO item. If failure, rollback and report error
    error = False
    body = {}
    try: 
       newTodo = Todo( description=newTodoItem )
+      newTodo.todolist_id = listId
 
       # add the new todo item to the db and commit
       db.session.add( newTodo )
@@ -98,6 +128,16 @@ def deleteTodo(todo_id):
    else:
       return jsonify({ 'success': True })
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+   return render_template( 
+      'index.html', 
+      lists = TodoList.query.all(),
+      active_list = TodoList.query.get(list_id),
+      todos = Todo.query.filter_by(todolist_id=list_id).order_by('id')
+      .all()
+   )
+
 @app.route('/')
 def index():
-   return render_template( 'index.html', data = Todo.query.order_by('id').all() )
+   return redirect(url_for('get_list_todos', list_id=1))
